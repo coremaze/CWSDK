996a286a30143dd09f5115a2fb29867b531f8857
yangzhi <@4F!xZpJwly&KbWq>
2019-10-09 21:03:23 +0800

    Add world_map cursor_position

--- cwmods/cube/WorldMap.h
+++ cwmods/cube/WorldMap.h
@@ -2,13 +2,16 @@
 #define WORLDMAP_H
 
 #include "../IDA/types.h"
+#include "../common/LongVector3.h"
 #include <windows.h>
 
 namespace cube {
 class WorldMap {
     public:
         void *vtable;
-        _BYTE gap8[408];
+        _BYTE gap8[248];
+        LongVector3 cursor_position;
+        _BYTE gap9[136];
         CRITICAL_SECTION critical_section_0;
         CRITICAL_SECTION critical_section_1;
     };
